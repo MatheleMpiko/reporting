d6468c3e61ea866510b4732eb72d0416964bc846
MatheleMpiko <mpikomathele@gmail.com>
2023-09-24 11:44:07 -0700

    user-profile

--- src/app/Services/reporttext.service.ts
+++ src/app/Services/reporttext.service.ts
@@ -16,7 +16,7 @@ export class ReporttextService {
   }
 
   read_reports() {
-    const booking = [];
+    const report = [];
     return this.firestore.collection(this.collectionName).snapshotChanges();
   }
 
--- src/app/admin-dashoard/admin-dashoard.page.ts
+++ src/app/admin-dashoard/admin-dashoard.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { ReportData } from '../Models/report-data';
+import { ReporttextService } from '../Services/reporttext.service';
+import { FormBuilder } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-admin-dashoard',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashoardPage implements OnInit {
 
-  constructor() { }
+  @Input() booking?: ReportData;
+  reportList! : any[];
+  reportData: ReportData = {
+    report: '',
+    category: '',
+    date: new Date,
+    location: '',
+  };
+  message = '';
+
+  constructor(private reportService: ReporttextService, public formBuilder: FormBuilder, private firestore: AngularFirestore) { }
 
   ngOnInit() {
+    this.message='';
+
+    this.reportService.read_reports().subscribe((data: any) => {
+      this.reportList = data.map((e: any) => {
+        return {
+          report: e.payload.doc.data()['report'],
+          category: e.payload.doc.data()['category'],
+          date: e.payload.doc.data()['date'],
+          location: e.payload.doc.data()['location']
+        };
+      })
+      console.log(this.reportList);
+    });
   }
 
 }
